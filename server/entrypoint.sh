2e07af1dc49a9d3e7e3b22d506932beaade3f86a
Enrico Franke <info@ezfox.ai>
2026-09-12 15:26:52 +0200

    Add multi-service deployment compose with Cloudflare Tunnel, multi-stage Django image

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python manage.py makemigrations
+mkdir -p /app/data
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 gunicorn --bind :8000 --workers 3 djangoproj.wsgi
